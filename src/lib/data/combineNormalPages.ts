36cce9b1714138d4232772ab515f144e1bea5708
PAD <167355314+spiritney@users.noreply.github.com>
2024-04-19 04:38:44 +0100

    up

--- src/lib/data/combineNormalPages.ts
+++ src/lib/data/combineNormalPages.ts
@@ -22,28 +22,32 @@ export const combineNormalPages = async () => {
             const fileName = fileNames[index];
             // Check if the file is a JSON file
             if (fileNames[index].endsWith('.json')) {
-                const filePath = `${directoryPath}/${fileNames[index]}`; // Construct full file path
                 const numberPage = Number(fileNames[index].split(".")[0].split("page")[1])
 
-                console.log(`i: ${index}, normal: ${numberPage}`);
-                // Read the file content
-                const jsonData = await fs.readFile(filePath, 'utf-8'); // Read as UTF-8
-
-                // Parse the JSON data (assuming it's valid JSON)
-                const pageData = JSON.parse(jsonData);
-
-                // save the page
-                const targetPath = path.join(directoryToPagePath, `${fileNames[index - flagCounter]}`); // Combine into pageX.json
-                // Write combined data to target file
-                await fs.writeFile(targetPath, JSON.stringify(pageData, null, 2), 'utf-8');
+                // if (!mappers[flagCounter]) {
+                //     continue;
+                // }
 
                 if (mappers.includes(numberPage)) {
                     console.log(`i: ${index}, combine: ${numberPage}`);
                     await combineTwoPagesInOne(numberPage);
                     flagCounter++
-                    index = index - flagCounter
+                    index++
                     continue;
+                } else {
+                    const filePath = `${directoryPath}/${fileNames[index]}`; // Construct full file path
+                    console.log(`i: ${index}, normal: ${numberPage}`);
+                    // Read the file content
+                    const jsonData = await fs.readFile(filePath, 'utf-8'); // Read as UTF-8
+                    // Parse the JSON data (assuming it's valid JSON)
+                    const pageData = JSON.parse(jsonData);
+                    // save the page
+                    const targetPath = path.join(directoryToPagePath, `${fileNames[index - flagCounter]}`); // Combine into pageX.json
+                    // Write combined data to target file
+                    await fs.writeFile(targetPath, JSON.stringify(pageData, null, 2), 'utf-8');
+
                 }
+
             } else {
                 console.log(`Skipping non-JSON file: ${fileName}`); // Informative message
             }
